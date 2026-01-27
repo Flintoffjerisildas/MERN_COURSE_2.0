6e17e069a9a17605c8006e493ec24de204e41629
Flintoffjerisildas <145636771+Flintoffjerisildas@users.noreply.github.com>
2026-01-27 09:09:18 +0530

    connections done

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: "https://mern-course-2-0-todoapp.vercel.app/",
         credentials: true,
     }
 ));
